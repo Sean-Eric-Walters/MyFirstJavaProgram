ada88aa5c9913218fb0d2492a3e7358e31eaa92d
Sean-Eric-Walters <seanericwalters@gmail.com>
2021-08-01 07:53:08 -0700

    init

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("my name is Sean Eric Walters"
+        +"\nI am 45 years old"
+        +"\nMy home town is Mesa, AZ");
+    }
+}
